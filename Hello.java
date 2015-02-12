819ff5af0c166b963d868949fc313fd1d4f0645d
Isaac Garcia <irg003@ucsd.edu>
2015-02-11 16:23:51 -0800

    Added a comment

--- Hello.java
+++ Hello.java
@@ -2,7 +2,8 @@ public class Hello
 {
     public static void main( String argv[] )
     {
-        String name = "World";
+        // Default is "World"
+	String name = "World";
         if ( argv.length != 0 )
         {
             name = argv[0];
